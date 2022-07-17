250ecf3d6d7614af3b7fecb0ccda12ee00188dc5
Michael Hooi <michael.r.hooi@gmail.com>
2022-07-17 16:22:41 -0400

    missed a few angle to value changes

--- servo.py
+++ servo.py
@@ -13,7 +13,6 @@ class Servo(gpiozero.Servo):
   def __init__(self, pin, lo=0, hi=np.pi):
     super().__init__(pin, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
     self.lo = lo; self.hi = hi
-    self.angle = 0
     self.lock = Lock()
 
   def set_value(self, new_value, delay=0.1):
@@ -23,9 +22,8 @@ class Servo(gpiozero.Servo):
       self.value = v
       time.sleep(delay)
     self.value = new_value
-    self.angle = angle
     self.lock.release()
 
   def set_value_threaded(self, value, delay=0.1):
-    t = Thread(target=self.set_value, args=(angle, delay,))
+    t = Thread(target=self.set_value, args=(value, delay,))
     t.start()
