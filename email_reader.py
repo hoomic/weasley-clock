51a8649fdd9a0b4883590fd4b2b3216408328512
Michael Hooi <michael.r.hooi@gmail.com>
2022-07-17 20:44:15 -0400

    Catch exceptions when trying to read email and continue

--- email_reader.py
+++ email_reader.py
@@ -13,20 +13,23 @@ class EmailReader():
     print("Successfully logged into email!")
 
   def read_email(self):
-    print("Reading email")
-    status, n_emails = self.mail.select('inbox')
-    n_emails = int(n_emails[0])
-    # if there are no emails, return empty list
-    if n_emails == 0:
-      return []
+    try:
+      status, n_emails = self.mail.select('inbox')
+      n_emails = int(n_emails[0])
+      # if there are no emails, return empty list
+      if n_emails == 0:
+        return []
 
-    #loop through emails
-    for i in range(n_emails):
-      # read the email
-      data = self.mail.fetch(str(i+1), '(RFC822)')[1][0][1]
-      # parse the data
-      msg = email.message_from_string(str(data, 'utf-8'))
-      #yield the message
-      yield msg
-      # delete the message
-      self.mail.store(str(i+1), "+FLAGS", "\\Deleted")
+      #loop through emails
+      for i in range(n_emails):
+        # read the email
+        data = self.mail.fetch(str(i+1), '(RFC822)')[1][0][1]
+        # parse the data
+        msg = email.message_from_string(str(data, 'utf-8'))
+        #yield the message
+        yield msg
+        # delete the message
+        self.mail.store(str(i+1), "+FLAGS", "\\Deleted")
+    except Exception as e:
+      print(e)
+      return []
