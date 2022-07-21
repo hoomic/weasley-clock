7e2e2501a199752a51df8d820d3fbc2ec86a0238
Michael Hooi <michael.r.hooi@gmail.com>
2022-07-21 12:07:57 -0400

    Add documentation for weasley_clock.py and add 'lost' logic

--- weasley_clock.py
+++ weasley_clock.py
@@ -26,14 +26,47 @@ logger.addHandler(fh)
 from email_reader import EmailReader
 email_reader = EmailReader()
 
+class Location():
+  def __init__(self, loc, timestamp):
+    self.loc = loc
+    self.timestamp = timestamp
+
 class WeasleyClock():
+  """
+  A class for Weasley Clock logic
+
+  ...
+
+  Attributes
+  ----------
+  hands : dict(str, Servo) 
+      a mapping from a person to their corresponding servo
+  last_loc : dict(str, Location)
+      a mapping from a person to their last location
+
+  Methods
+  -------
+  run()
+      The main loop checking for commands to be sent to the servos
+  process_command(person, loc)
+      Calculates the servo position and sends the command to the servo
+  update()
+      Runs periodically for any logic that isn't commanded from the email server         
+  """
   def __init__(self):
     self.hands = dict()
     self.hands['zach'] = Servo(SHORT_HAND_PIN)
     self.hands['penny'] = Servo(LONG_HAND_PIN)
+    self.last_loc = dict()
     self.run()
 
   def run(self):
+    """
+    The main loop checking for commands to be sent to the servos
+
+    Check for emails that follow the person,location,[entered/exited] pattern
+    and send those commands to the servos
+    """
     while True:
       # Check to see if a location change has been triggered
       for msg in email_reader.read_email():
@@ -60,13 +93,26 @@ class WeasleyClock():
       sleep(10)
 
   def process_command(self, person, loc):
+    """
+    Calculates the servo position and sends the command to the servo
+    """
     logger.info("Processing Command: {},{}".format(person, loc))
     index = locations.index(loc.lower())
     value= 2 * index / len(locations) - 1
     self.hands[person].set_value_threaded(value)
+    self.last_loc[person] = Location(loc, datetime.now())
 
   def update(self):
-    pass
+    """
+    Runs periodically for any logic that isn't commanded from the email server
+
+    if the last location of a person is 'travel', and they have been in that state
+    for more than 4 hours, then consider them 'lost'
+    """
+    now = datetime.now()
+    for person, loc in self.last_loc.items():
+      if loc.loc == 'travel' and (now - loc.timestamp) > timedelta(hours=4):
+        self.process_command(person, 'lost')
 
 if __name__ == '__main__':
   weasley_clock = WeasleyClock()
